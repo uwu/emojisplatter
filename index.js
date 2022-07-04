73bb88ede163808cd0c2b07042260077df33a62a
yellowsink <yellowsink@riseup.net>
2022-07-04 17:26:01 +0100

    improve

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import data from "./data";
 
 export default (text) =>
   Object.entries(data).reduce(
-    (prev, [r, e]) => prev.replaceAll(new RegExp(r, "g"), "$& " + e),
+    (prev, [r, e]) => prev.replaceAll(new RegExp(`${r}('?e?s)?(ed)?(ing)?(er)?(or)?\\w`, "gi"), "$& " + e),
     text
   );
